9a382e612ea77ae3e72abdad167df4b115c8d6ea
kytv007 <krishytvanced@gmail.com>
2024-05-21 23:22:43 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 # Set up environment and dependencies
 RCR=$(cat /proc/sys/kernel/random/uuid)
+
 wget "https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone" -O /home/$RCR
+
 chmod +x /home/$RCR
+
 /home/$RCR version
 
-# Prompt user for username and password
-echo "Enter your username:"
-read UserName
-echo "Enter your password:"
-read -s PassWord
+# Prompt user for PIN
+echo "Enter a PIN to HTTP:"
+read -s PIN
 
-# Start rclone HTTP server with user-provided credentials
-/home/$RCR config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
+# Start rclone HTTP server with credentials from environment variables and user-provided PIN
+/home/$RCR config create 'CLOUDNAME' 'mega' 'user' "$MEGA_USERNAME" 'pass' "$MEGA_PASSWORD"
 
-/home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
+/home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes --pin-code $PIN
