e133cd45c2b8e94a148fb885127e08e0bc025f49
kytv007 <krishytvanced@gmail.com>
2024-05-21 19:18:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
 
 # Set up environment and dependencies
 RCR=$(cat /proc/sys/kernel/random/uuid)
-touch checkquota.log
-log=checkquota.log
 wget "https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone" -O /home/$RCR
 chmod +x /home/$RCR
-touch /Mega-index-heroku/quota-bypass/checkquota.log
 /home/$RCR version
 /home/$RCR config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 
